15ef128eefd3257229beb7eb3c592f1a0ecadbba
FuryxSAMURAI <111450833+FuryxSAMURAI@users.noreply.github.com>
2023-01-19 23:30:17 +0200

    first commit  BETA

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function openMenu() {
+    menu2 = document.querySelector('.header__menu-links');
+    menu2.style.display = 'block';
+    menuOpenClose.style.display = 'block';
+}
+function closeMenu() {
+    menu = document.querySelector('.header__menu');
+    menu.style.display = 'block';
+    menuOpenClose.style.display = 'none';
+}
